82aeb633493146da634a79e6d9e4d4978b15d53d
haotian <haotian@pop-os.localdomain>
2019-03-15 22:44:29 -0400

    ingest input script

---
+++ ingest_input.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+def readFile():
+    paramsFile = open("input.txt")
+    paramFileLines = paramsFile.readlines()
+    return paramFileLines
+
+def getExpectedNumPeaks():
+    lines = readFile()
+    print(lines[0])
+    return lines[0]
+
+def getExpectedPeaks():
+    result = []
+    lines = readFile()
+    for i in range(1, len(lines)):
+        line = lines[i].split(" ")
+        result.append((line[0], line[1][0:len(line[1]) - 1]))
+    print(result)
+    return result
+
+
+
+getExpectedNumPeaks()
+getExpectedPeaks()
