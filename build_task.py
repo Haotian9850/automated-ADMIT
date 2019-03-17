da885a140a37faf5493842d29f9dbdb7ca472057
haotian <haotian@pop-os.localdomain>
2019-03-17 17:31:56 -0400

    clean up

--- build_task.py
+++ build_task.py
@@ -27,23 +27,6 @@ def assemble(i, numsigma, minchan, maxchan):
 
 def runAllProjects():
     for i in range(0, len(allParams), 1):
-        '''
-        p = admit.Project('molecular-line.admit' + '_' + str(i), dataserver=True)
-        t0  = p.addtask(admit.Ingest_AT(file=fileName))
-        t1  = p.addtask(admit.CubeStats_AT(ppp=True), [t0])
-        t2  = p.addtask(admit.Moment_AT(mom0clip=2.0, numsigma=[0.5]), [t0, t1])
-        t3  = p.addtask(admit.CubeSpectrum_AT(), [t0, t2])
-        t4 = p.addtask(admit.LineID_AT(
-            vlsr = 8.0, #fixed value
-            numsigma = allParams[i][0].item(),
-            minchan = allParams[i][1],
-            maxgap = allParams[i][2], #default value
-            identifylines = True,
-            allowexotics = True,
-            recomblevel = 'deep',
-        ), [t1, t3])
-        p.run() #only to be executed within casa shell
-        '''
         newProject = assemble(i, allParams[i][0].item(), allParams[i][1], allParams[i][2])
         newProject.run()
 
