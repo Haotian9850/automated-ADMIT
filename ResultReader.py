f10654f87d6e030cf99c55cb18bb34738d541721
haotian <haotian@pop-os.localdomain>
2019-03-23 20:24:27 -0400

    more comments

--- ResultReader.py
+++ ResultReader.py
@@ -3,12 +3,24 @@ import json
 
 
 def getAllLineProfile(fileName):
+    """
+    Args:
+        fileName: name of input admit result file (String)
+    Retuens:
+        all information on all identified molecular lines (nested list)
+    """
     with open(fileName, 'r') as jsonFile:
         allResult = json.load(jsonFile)
         lines = allResult["linetable"]["lines"]
     return lines
 
 def parseFreq(lines):
+    """
+    Args:
+        lines: all information on all identified molecular lines (nested list)
+    Returns:
+        frequency range of all identified molecular lines (nested list)
+    """
     result = []
     for line in lines:
         frequency = line["frequency"]
@@ -17,6 +29,12 @@ def parseFreq(lines):
     return result
 
 def parsePeakIntensity(lines):
+    """
+    Args:
+        lines: all information on all identified molecular lines (nested list)
+    Returns:
+        list of peak intensity of all identified molecular lines (list)
+    """
     result = []
     for line in lines:
         peakIntensity = line["peakintensity"]
@@ -25,6 +43,12 @@ def parsePeakIntensity(lines):
     return result
 
 def parseChannels(lines):
+    """
+    Args:
+        lines: all information on all identified molecular lines (nested list)
+    Returns:
+        list of channel ranges of all identified molecular lines (nestde list)
+    """
     result = []
     for line in lines:
         startChan = line["startchan"]
